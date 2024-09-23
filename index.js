21577dd63db1cc4bfb4753bad295214f3d613898
Jason Le <danglm@aht-asia.com>
2024-09-23 17:11:18 +0700

    change server files

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-const express = require('express');
+// server.js
+//
+// Use this sample code to handle webhook events in your integration.
+//
+// 1) Paste this code into a new file (server.js)
+//
+// 2) Install dependencies
+//   npm install stripe
+//   npm install express
+//
+// 3) Run the server on http://localhost:4242
+//   node server.js
+
+// The library needs to be configured with your account's secret key.
+// Ensure the key is kept out of any version control system you might be using.
+const stripe = require("stripe")(
+  "sk_test_51Q0EJJLIMEwBaev46MPBjU4XTiedyVCZF4QxuJjWgWXBvH3nVvpccxz8uZghY1DWwXqUQXfVCTHDRq1Spzs49QdY00UUkPJhvx"
+);
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Vercel!');
-});
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_a6e2c243d23695ebe5d62a653fc3a398831d6d70d3b09c7e659df7dbb7bf1751";
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
+
+app.listen(5000, () => console.log("Running on port 5000"));
